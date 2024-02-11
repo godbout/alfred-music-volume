2517b50b2a0599aabc40180afc5ad8033dab3136
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2024-02-11 15:11:28 +0800

    reorganize repo

---
+++ Workflow/src/music.zsh
@@ -0,0 +1,27 @@
+local APP_NAME=Music
+
+autoload is-at-least
+if ! is-at-least 10.15 $(sw_vers -productVersion); then
+    APP_NAME=iTunes
+fi
+
+local opt=$1
+shift
+case "$opt" in
+    vol)
+        local new_volume volume=$(osascript -e "tell application \"$APP_NAME\" to get sound volume")
+        if [[ $# -eq 0 ]]; then
+            echo "Current volume is ${volume}."
+            return 0
+        fi
+        case $1 in
+            up) new_volume=$((volume + 10 < 100 ? volume + 10 : 100)) ;;
+            down) new_volume=$((volume - 10 > 0 ? volume - 10 : 0)) ;;
+            <0-100>) new_volume=$1 ;;
+            *) echo "'$1' is not valid. Expected <0-100>, up or down."
+               return 1 ;;
+        esac
+        opt="set sound volume to ${new_volume}"
+        ;;
+esac
+osascript -e "tell application \"$APP_NAME\" to $opt"
