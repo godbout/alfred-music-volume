3999289066ad37901cfe6e8a147453dd7d7dbbd0
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2024-02-13 12:59:23 +0800

    clean code

--- Workflow/resources/scripts/music.zsh
+++ Workflow/resources/scripts/music.zsh
@@ -5,23 +5,18 @@ if ! is-at-least 10.15 $(sw_vers -productVersion); then
     APP_NAME=iTunes
 fi
 
-local opt=$1
-shift
-case "$opt" in
-    vol)
-        local new_volume volume=$(osascript -e "tell application \"$APP_NAME\" to get sound volume")
-        if [[ $# -eq 0 ]]; then
-            echo "Current volume is ${volume}."
-            return 0
-        fi
-        case $1 in
-            up|u) new_volume=$((volume + 10 < 100 ? volume + 10 : 100)) ;;
-            down|d) new_volume=$((volume - 10 > 0 ? volume - 10 : 0)) ;;
-            <0-100>) new_volume=$1 ;;
-            *) echo "'$1' is not valid. Expected <0-100>, up or down."
-               return 1 ;;
-        esac
-        opt="set sound volume to ${new_volume}"
-        ;;
+local new_volume volume=$(osascript -e "tell application \"$APP_NAME\" to get sound volume")
+if [[ $# -eq 0 ]]; then
+    echo "Current volume is ${volume}."
+    return 0
+fi
+case $1 in
+    up|u) new_volume=$((volume + 10 < 100 ? volume + 10 : 100)) ;;
+    down|d) new_volume=$((volume - 10 > 0 ? volume - 10 : 0)) ;;
+    <0-100>) new_volume=$1 ;;
+    *) echo "'$1' is not valid. Expected <0-100>, up/u or down/d."
+       return 1 ;;
 esac
+opt="set sound volume to ${new_volume}"
+
 osascript -e "tell application \"$APP_NAME\" to $opt"
